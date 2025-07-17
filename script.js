f173a57f7e9207a864a847fcc97345f77bdf8cf4
preethisara01 <sarathapreethi373@gmail.com>
2025-07-17 15:27:04 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
-/*
-var val=67;
-let val2=34;
-console.log(val);
-console.log("45" == 45);
-console.log("46" == 76);
-
-const prompt=require("prompt-sync")();
-
-let age=prompt("enter the age");
-let UrName=prompt("enter your name:");
-
-console.log("UrName");
-console.log("age");
-
-var age=prompt("enter the age");
-if(age>=70){
-  console.log("retired");
-}
-else{
- console.log("working");
-}
-console.log(age)
-*/
+console.log("I");
+console.log("It is the useful of the program");
